4c47c6d411a2b9d0bcfe13263d69b7836c4ff055
emanueljsweb <75799134+emanueljsweb@users.noreply.github.com>
2023-10-13 21:33:43 -0300

    Acrescenta documentação para a classe Sprite e seus métodos.

--- src/Sprite.java
+++ src/Sprite.java
@@ -3,6 +3,14 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * A classe Sprite é respponsável por armazenar
+ * a imagem correspondente a folha de sprites
+ * que pode ser utilizada em animações. Essa classe
+ * encapsula os atributos e métodos necessários para
+ * obter as texturas a partir de uma imagem PNG.
+ * @see Animation
+ */
 public class Sprite {
 
     private BufferedImage spritesheet;
@@ -11,6 +19,10 @@ public class Sprite {
     private int qtdLinhas;
     private static int tileSize = 128;
 
+    /**
+     * Instancia um novo objeto da classe e inicializa seus principais atributos.
+     * @param caminhoArquivo caminho do arquivo de imagem da spritesheet.
+     */
     public Sprite(String caminhoArquivo) {
 
         // Importa a imagem correspondente ao spritesheet
@@ -27,16 +39,31 @@ public class Sprite {
 
     }
 
+    /**
+     * Retorna a imagem correspondente a uma das texturas disponíveis.
+     * @param xGrid posição X do grid da spritesheet
+     * @param yGrid posição Y do grid da spritesheet.
+     * @return imagem correspondente a posição solicitada.
+     * @see BufferedImage
+     */
     public BufferedImage getSprite(int xGrid, int yGrid) {
 
         return spritesheet.getSubimage(xGrid * tileSize, yGrid * tileSize, tileSize, tileSize);
 
     }
 
+    /**
+     * Retorna a quantidade de linhas da spritesheet.
+     * @return quantidade de linhas da spritesheet.
+     */
     public int getQtdLinhas() {
         return qtdLinhas;
     }
 
+    /**
+     * Retorna a quantidade de frames por linha da spritesheet.
+     * @return quantidade de frames por linha da spritesheet.
+     */
     public int getFramesPorLinha() {
         return qtdFrames / qtdLinhas;
     }

761f42a8acce3a71dd0571bf8c8e9ceff05bc1f3
emanueljsweb <75799134+emanueljsweb@users.noreply.github.com>
2023-10-13 21:06:42 -0300

    Acrescenta documentação para a classe Guerreiro e seus métodos.

--- src/Guerreiro.java
+++ src/Guerreiro.java
@@ -1,23 +1,47 @@
 import java.util.Random;
 
+/**
+ * A classe Guerreiro permite a criação de um
+ * jogador. Essa classe é responsável por encapsular
+ * os atributos em métodos necessários para o gerenciamento
+ * da gameplay do jogador, como seu nome e sua quantidade de vidas.
+ */
 public class Guerreiro {
 
     private String nome;
     private int qtdVidas;
 
+    /**
+     * Instancia um novo objeto da classe, inicializando
+     * seus principais atributos e definindo sua quantidade
+     * de vida por sorteio.
+     * @param nome do guerreiro
+     */
     public Guerreiro(String nome) {
         this.nome = nome;
         setVidas();
     }
 
+    /**
+     * Retorna o nome do guerreiro.
+     * @return nome do guerreiro
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Retorna a quantidade de vidas do guerreiro.
+     * @return quantidade de vidas do guerreiro.
+     */
     public int getQtdVidas() {
         return qtdVidas;
     }
 
+    /**
+     * Define a quantidade de vidas do guerreiro entre [9, 12]
+     * por meio de um sorteio utilizando um número randômico.
+     */
     private void setVidas(){
         Random random = new Random();
         int min = 9;
@@ -25,10 +49,17 @@ public class Guerreiro {
         this.qtdVidas = random.nextInt(max - min + 1) + min;
     }
 
+    /**
+     * Aumenta a quantidade de vidas do guerreiro em 1 unidade.
+     */
     public void aumentarVida() {
         this.qtdVidas++;
     }
 
+    /**
+     * Diminui a quantidade de vidas do guerreiro em 1 unidade e
+     * alerta essa mudança ao jogador.
+     */
     public void diminuirVida(){
        this.qtdVidas --;
 
@@ -39,6 +70,10 @@ public class Guerreiro {
        }
     }
 
+    /**
+     * Lê uma String do apelo do jogador por uma vida extra.
+     * @return o apelo do jogador
+     */
     public String vidaExtra(){
 
        return InOut.leString("Implore pela sua VIDA HAHAHAHA!");
